8e02f191b10271e99d6e4f00e045e8ea6dab193e
Chris Cannam <cannam@all-day-breakfast.com>
2019-12-13 13:54:27 +0000

    Test logic

--- installer.cpp
+++ installer.cpp
@@ -1,5 +1,31 @@
 
+#include <QApplication>
+#include <QString>
+#include <QFile>
+#include <QDir>
+
+#include <iostream>
+using namespace std;
+
 int main(int argc, char **argv)
 {
+    QApplication app(argc, argv);
+    QDir dir(":out/");
+    auto entries = dir.entryList({ "*.so", "*.dll", "*.dylib" });
+
+    for (auto e: entries) {
+        cerr << e.toStdString() << endl;
+    }
+
+    QString target = QDir::homePath() + "/Library/Audio/Plug-Ins/Vamp/";
+
+    for (auto e: entries) {
+        QFile f(":out/" + e);
+        if (!f.copy(target + e)) {
+            cerr << "Failed to copy " << e.toStdString()
+                 << " to target " << (target + e).toStdString() << endl;
+        }
+    }
+    
     return 0;
 }
