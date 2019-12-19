ec9a36d92ed8dd508a86b1ffc9b71eb33303974a
Chris Cannam <cannam@all-day-breakfast.com>
2019-12-19 13:44:39 +0000

    Use actual plugin path

--- installer.cpp
+++ installer.cpp
@@ -4,11 +4,19 @@
 #include <QFile>
 #include <QDir>
 
+#include <vamp-hostsdk/PluginHostAdapter.h>
+
 #include <iostream>
 using namespace std;
 
 int main(int argc, char **argv)
 {
+    auto pathList = Vamp::PluginHostAdapter::getPluginPath();
+    if (pathList.empty()) {
+        cerr << "Failed to look up Vamp plugin path" << endl;
+        return 1;
+    }
+
     QApplication app(argc, argv);
     QDir dir(":out/");
     auto entries = dir.entryList({ "*.so", "*.dll", "*.dylib" });
@@ -17,19 +25,24 @@ int main(int argc, char **argv)
         cerr << e.toStdString() << endl;
     }
 
-    QString target = QDir::homePath() + "/Library/Audio/Plug-Ins/Vamp/";
+    auto firstPath = *pathList.begin();
+    QString target = QString::fromUtf8(firstPath.c_str(), firstPath.size());
 
     for (auto e: entries) {
         QFile f(":out/" + e);
-        if (!f.copy(target + e)) {
+        QString destination = target + "/" + e;
+        cerr << "Copying " << e.toStdString() << " to "
+             << destination.toStdString() << "..." << endl;
+        if (!f.copy(destination)) {
             cerr << "Failed to copy " << e.toStdString()
-                 << " to target " << (target + e).toStdString() << endl;
+                 << " to target " << destination.toStdString() << endl;
 	    continue;
         }
-	if (!QFile::setPermissions(target + e, 
-				   QFile::ReadOwner | QFile::WriteOwner | 
-				   QFile::ExeOwner | QFile::ReadGroup | 
-				   QFile::ReadOther)) {
+	if (!QFile::setPermissions
+            (destination,
+             QFile::ReadOwner | QFile::WriteOwner | QFile::ExeOwner |
+             QFile::ReadGroup | QFile::ExeGroup |
+             QFile::ReadOther | QFile::ExeOther)) {
             cerr << "Failed to set permissions on " << e.toStdString() << endl;
 	    continue;
 	}
--- installer.pro
+++ installer.pro
@@ -47,7 +47,23 @@ qrc_cpp.commands = rcc $$qrc.target -o $$qrc_cpp.target
 QMAKE_EXTRA_TARGETS += qrc_cpp
 PRE_TARGETDEPS += $$qrc_cpp.target
 
-SOURCES += installer.cpp $$qrc_cpp.target
+SOURCES += \
+        installer.cpp \
+        $$qrc_cpp.target \
+        vamp-plugin-sdk/src/vamp-hostsdk/Files.cpp \
+        vamp-plugin-sdk/src/vamp-hostsdk/host-c.cpp \
+        vamp-plugin-sdk/src/vamp-hostsdk/PluginBufferingAdapter.cpp \
+        vamp-plugin-sdk/src/vamp-hostsdk/PluginChannelAdapter.cpp \
+        vamp-plugin-sdk/src/vamp-hostsdk/PluginHostAdapter.cpp \
+        vamp-plugin-sdk/src/vamp-hostsdk/PluginInputDomainAdapter.cpp \
+        vamp-plugin-sdk/src/vamp-hostsdk/PluginLoader.cpp \
+        vamp-plugin-sdk/src/vamp-hostsdk/PluginSummarisingAdapter.cpp \
+        vamp-plugin-sdk/src/vamp-hostsdk/PluginWrapper.cpp \
+        vamp-plugin-sdk/src/vamp-hostsdk/RealTime.cpp
+
+linux* {
+   LIBS += -ldl
+}
 
 macx* {
     QMAKE_POST_LINK += deploy/osx/deploy.sh $$shell_quote($$TARGET)
