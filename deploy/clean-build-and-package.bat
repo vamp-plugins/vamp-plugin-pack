02936d6de6922b3933dd6977cbdf7ae318f5cb51
Chris Cannam <cannam@all-day-breakfast.com>
2024-07-31 10:35:46 +0100

    We won't have 32-bit this time

--- deploy/clean-build-and-package.bat
+++ deploy/clean-build-and-package.bat
@@ -14,12 +14,6 @@ set VERSION=%VERSION:"=%
 
 @echo Proceeding
 
-@echo Building 32-bit
-del /q /s out
-del /q /s build_win32
-call .\deploy\win32\build-32.bat sign
-if %errorlevel% neq 0 exit /b %errorlevel%
-
 @echo Building 64-bit
 del /q /s out
 del /q /s build_win64
@@ -27,7 +21,6 @@ call .\deploy\win64\build-64.bat sign
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 mkdir packages
-copy "build_win32\release\Vamp Plugin Pack Installer.exe" "packages\Vamp Plugin Pack Installer %VERSION% 32-bit.exe"
 copy "build_win64\release\Vamp Plugin Pack Installer.exe" "packages\Vamp Plugin Pack Installer %VERSION%.exe"
 
 @echo(
